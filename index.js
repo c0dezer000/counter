3cbdc74294fa816ad8c5f260895e69940ff2e677
Jerome <fernandezjerome763@gmail.com>
2024-08-10 03:51:12 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const counts = document.getElementById("counts")
+const decreaseBtn = document.getElementById("decrease")
+const resetBtn = document.getElementById("resets")
+const increaseBtn = document.getElementById("increase")
+
+

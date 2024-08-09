d09b8c77afc9fbd873f9df396581e6575cbb1bb0
Jerome <fernandezjerome763@gmail.com>
2024-08-10 04:15:00 +0800

    working state

--- index.js
+++ index.js
@@ -2,5 +2,33 @@ const counts = document.getElementById("counts")
 const decreaseBtn = document.getElementById("decrease")
 const resetBtn = document.getElementById("resets")
 const increaseBtn = document.getElementById("increase")
+const countColor = document.querySelector("counts")
 
 
+let count = 0 
+function increase(){
+    count ++
+    counts.textContent = count
+    colorChecker()
+}
+function decrease(){
+   count--
+   counts.textContent = count
+   colorChecker()
+}
+function resets(){
+    count = 0
+    counts.textContent = "0"
+    colorChecker()
+}
+function colorChecker(){
+    if(count > 0){
+        counts.style.color = "green"
+    }
+    else if(count < 0){
+        counts.style.color = "red"
+    }
+    else{
+        counts.style.color = "black"
+    }
+}
